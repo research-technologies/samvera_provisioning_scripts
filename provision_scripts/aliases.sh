b29e436adbebcb54c5fd8e16c2d954a9b3ce00c8
Julie Allinson <julie.allinson@london.ac.uk>
2018-10-22 17:24:10 +0100

    add handy aliases

---
+++ provision_scripts/aliases.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+USER=$(echo $APPLICATION_USER)
+APP_KEY=$(echo $APPLICATION_KEY)
+
+# Add to main user
+echo "alias hy='sudo -u $USER -i'" >> ~/.bash_profile
+echo "alias hyst='sudo systemctl start puma && sudo systemctl start sidekiq'" >> ~/.bash_profile
+echo "alias hystop='sudo systemctl start puma && sudo systemctl start sidekiq'" >> ~/.bash_profile
+echo "alias hyre='sudo systemctl start puma && sudo systemctl start sidekiq'" >> ~/.bash_profile
+echo "alias hytail='tail -f /var/log/$APP_KEY/"$APP_KEY"_production.log'" >> ~/.bash_profile
+source ~/.bash_profile
+
+# Add to application user
+sudo -u $USER echo "alias hylib='cd /var/lib/$APP_KEY'" | sudo tee -a /home/$USER/.bash_profile
+sudo -u $USER echo "alias hytail='tail -f /var/log/$APP_KEY/"$APP_KEY"_production.log'" | sudo tee -a /home/$USER/.bash_profile
+sudo -u $USER cat /home/$USER/.bash_profile
